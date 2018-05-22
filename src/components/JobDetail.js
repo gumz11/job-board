4ba97ca491b69a43930612be7860270b07ba2e1f
Andrew Gomez <gomezandrewj@gmail.com>
2018-05-21 19:20:43 -0700

    Update detail page style

--- src/components/JobDetail.js
+++ src/components/JobDetail.js
@@ -3,29 +3,25 @@ import React from 'react';
 import Header from "./Header";
 import JobsMessage from './JobsMessage';
 
-class JobsDetail extends React.Component {
-    render() {
-        return (
-            <React.Fragment>
-                <Header onSearch={this.props.onSearch} />
-                <main className="jb-fill jb-row">
-                    {this.props.searching ? <JobsMessage /> : !this.props.job  && <JobsMessage error="Job not found." /> }
-                    <section className="jb-main jb-content">
-                        {this.props.job && 
-                            <React.Fragment>
-                                <h1>{this.props.job.title}</h1>
-                                <h4>{this.props.job.company} - {this.props.job.location} </h4>
-                                <br/>
-                                <h5> Posted: {this.props.job.created_at} - {this.props.job.type} </h5>
-                                <br/>
-                                <p dangerouslySetInnerHTML={{__html: this.props.job.description}} />
-                            </React.Fragment>
-                        }
-                    </section>
-                </main>
-            </React.Fragment>
-        );
-    }
-}
+const JobsDetail = (props) => (
+    <React.Fragment>
+        <Header onSearch={props.onSearch} />
+        <main className="jb-fill jb-row">
+            {props.searching ? <JobsMessage /> : !props.job  && <JobsMessage error="Job not found." /> }
+            <section className="jb-main jb-content">
+                {props.job && 
+                    <React.Fragment>
+                        <h1><a href={props.job.url}>{props.job.title}</a></h1>
+                        <h4>{props.job.company} - {props.job.location} </h4>
+                        <br/>
+                        <h5> Posted: {props.job.created_at} - {props.job.type} </h5>
+                        <br/>
+                        <div className="jb-html" dangerouslySetInnerHTML={{__html: props.job.description}} />
+                    </React.Fragment>
+                }
+            </section>
+        </main>
+    </React.Fragment>
+);
 
 export default JobsDetail;
--- src/components/MapMarker.js
+++ src/components/MapMarker.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link } from 'react-router-dom'
+import { Link } from 'react-router-dom';
 import L from 'leaflet';
 
 import markerIcon from 'leaflet/dist/images/marker-icon.png';
