c8a7973d0a9388192f58422110177fe7307ee058
Andrew Gomez <gomezandrewj@gmail.com>
2018-05-21 19:12:58 -0700

    optimize fetch request

--- src/components/App.js
+++ src/components/App.js
@@ -25,13 +25,13 @@ class App extends Component {
     }
     
     componentDidMount() {
-        this.search('', 0);
+        this.onSearch();
     }
 
     onSearch = (e, searchVal) => {
         searchVal = searchVal ? searchVal : '';
-        
-        e.preventDefault();
+        if (e) e.preventDefault();
+
         this.setState({
             searching: true,
             formSearch: searchVal,
@@ -39,16 +39,33 @@ class App extends Component {
             error: null
         });
 
-        this.search(searchVal, 0);
+        this.loopSearch(searchVal, 0);
+    }
+
+    loopSearch(searchVal, page) {
+        let promises = [],
+            i;
+        
+        for (i = page; i < page+6; i++) {
+            promises.push(this.search(searchVal, i));
+        }
+
+        Promise.all(promises).then((jobs) => {
+            // Any array was an empty result; we are done
+            if (jobs.find((j) => j.length === 0)) {
+                this.setState({ formSearch: '', searching: false, error: null });
+            } else {
+                this.loopSearch(searchVal, i);
+            }
+        });
     }
 
     search(searchVal, page) {
         let url = `http://localhost/api/jobs?search=${searchVal}&page=${page}`;
 
-        fetch(url)
+        return fetch(url)
             .then(r => r.json())
             .then((jobs) => {
-
                 if (jobs.error) {
                     throw new Error(jobs.message);
                 }
@@ -57,11 +74,7 @@ class App extends Component {
                     jobs: prevState.jobs.concat(jobs)
                 }));
 
-                if (jobs.length) {
-                    this.search(searchVal, ++page);
-                } else {
-                    this.setState({ formSearch: '', searching: false, error: null });
-                }
+                return jobs;
             })
             .catch((e) => {
                 this.setState({ 
@@ -81,19 +94,19 @@ class App extends Component {
                         this.state.formSearch ? <Redirect push to="/jobs" /> : <Home onSearch={this.onSearch} />
                     }/>
 
+                    <Route path="/job/:jobId" render={({match}) => 
+                        this.state.formSearch ? <Redirect push to="/jobs" /> :
+                            <JobDetail searching={this.state.searching} 
+                                    job={this.state.jobs.find((j) => j.id === match.params.jobId)}
+                                    onSearch={this.onSearch} />
+                    }/>
+
                     <Route path="/jobs" render={() => 
                         <JobsMap searching={this.state.searching} 
                             formSearch={this.state.formSearch}
                             jobs={this.state.jobs} 
                             onSearch={this.onSearch}
                             error={this.state.error} />
-                    }/>
-                    
-                    <Route path="/job/:jobId" render={({match}) => 
-                        this.state.formSearch ? <Redirect push to="/jobs" /> :
-                            <JobDetail searching={this.state.searching} 
-                                    job={this.state.jobs.find((j) => j.id === match.params.jobId)}
-                                    onSearch={this.onSearch} />
                     }/>
 
                 </React.Fragment>
